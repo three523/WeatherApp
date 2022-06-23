4d3b98be2e6111b0e188befe6a365a044c240961
three523 <thtree523@gmail.com>
2022-06-23 12:18:47 +0900

    WeatherApp First Commit

---
+++ WeatherApp/CityListCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  WeatherApp
+//
+//  Created by apple on 2022/06/23.
+//
+
+import UIKit
+
+class CityListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by apple on 2022/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cityListTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
