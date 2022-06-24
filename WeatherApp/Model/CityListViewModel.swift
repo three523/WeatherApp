b9f1dd1ada0ec3c7611290a09cf446100cb47579
three523 <thtree523@gmail.com>
2022-06-24 18:21:34 +0900

    도시별 날씨 출력 및 ui autolayout 조정

---
+++ WeatherApp/Model/CityListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CityListViewModel.swift
+//  WeatherApp
+//
+//  Created by apple on 2022/06/24.
+//
+
+import Foundation
+
+class CityListViewModel {
+    static var cityWeatherList: [CityWeather] = [CityWeather]()
+}
